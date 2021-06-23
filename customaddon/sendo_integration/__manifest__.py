7ecf2622bd9be231df38111b73e86ef928429c4b
Viet <11185573@st.neu.edu.vn>
2021-06-23 16:30:29 +0700

    update sendo categories

--- customaddon/sendo_integration/__manifest__.py
+++ customaddon/sendo_integration/__manifest__.py
@@ -16,7 +16,7 @@
         'wizard/connect_to_shop_wizard.xml',
         'views/sendo_seller_product_view.xml',
         'views/sendo_list_order_view.xml',
-        # 'views/sendo_categories_view.xml',
+        'views/sendo_categories_view.xml',
         # 'views/sendo_seller_view.xml',
         # 'views/assets_backend.xml',
     ],
